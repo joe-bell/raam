df9e2a90b2d864922e918905c3ed3b805943d9ad
joe-bell <joe@joebell.co.uk>
2020-10-14 19:43:08 +0300

    test: update and extend

--- packages/raam/test/flex.ts
+++ packages/raam/test/flex.ts
@@ -1,11 +1,21 @@
-import { useFlex } from "../dist";
+import { RaamTheme, useFlex } from "../dist";
+
+const theme: RaamTheme = {
+  space: [0, 4, 8, 16, 32, 64],
+  breakpoints: ["20em", "40em", "60em", "80em"],
+};
+const themeAlternative: RaamTheme = {
+  space: theme.space,
+  breakpoints: { small: "20em", medium: "40em", large: "60em", xlarge: "80em" },
+};
 
 describe("useFlex", () => {
-  test("renders as hStack by default", () => {
-    const sample = () => {
+  test("with themed responsive props", () => {
+    const example = () => {
       const flex = useFlex({
-        gap: ["1rem", null, "3rem"],
-        theme: { breakpoints: ["20em", "40em", "60em", "80em"] },
+        gap: [3, null, 4, "4rem"],
+        alignItems: [null, "flex-end", "flex-start"],
+        theme,
       });
 
       return {
@@ -14,18 +24,33 @@ describe("useFlex", () => {
       };
     };
 
-    console.log(sample());
-
-    // Should expect 1rem
-    // 3rem at 40em
-
-    expect(sample().parent).toEqual(
+    expect(example().parent).toEqual(
       expect.objectContaining({
         display: "flex",
-        "--raam-fg-t": expect.any(String),
-        "--raam-fg-l": expect.any(String),
-        "--raam-fg-offset": expect.any(String),
-        "--raam-fg": expect.any(String),
+        // due to hStack being default variant
+        "--raam-fg-t": `initial`,
+        "--raam-fg-l": `${theme.space[3]}px`,
+      })
+    );
+    expect(example().parent).toHaveProperty(
+      `@media screen and (min-width: ${theme.breakpoints[0]})`,
+      expect.objectContaining({
+        alignItems: "flex-end",
+      })
+    );
+    expect(example().parent).toHaveProperty(
+      `@media screen and (min-width: ${theme.breakpoints[1]})`,
+      expect.objectContaining({
+        "--raam-fg-t": `${theme.space[4]}px`,
+        "--raam-fg-l": `${theme.space[4]}px`,
+        alignItems: "flex-start",
+      })
+    );
+    expect(example().parent).toHaveProperty(
+      `@media screen and (min-width: ${theme.breakpoints[2]})`,
+      expect.objectContaining({
+        "--raam-fg-t": "4rem",
+        "--raam-fg-l": "4rem",
       })
     );
   });
