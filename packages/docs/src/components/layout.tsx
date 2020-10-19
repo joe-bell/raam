500ef9bb29283a58af37ab37932167b845e41a7b
joe-bell <joe@joebell.co.uk>
2020-10-19 14:30:12 +0300

    chore(docs): migrate to tailwindcss

--- packages/docs/src/components/layout.tsx
+++ packages/docs/src/components/layout.tsx
@@ -1,93 +1,89 @@
 /** @jsx jsx */
 import * as React from "react";
-import { jsx, Box, Container, Heading, NavLink, Link } from "theme-ui";
-import { Flexbox } from "@raam/theme-ui";
+import { jsx } from "theme-ui";
 import Head from "./head";
 import ToggleColorMode from "./toggle-color-mode";
 import config from "../config";
-
-const border = {
-  border: "thick",
-  borderColor: "border",
-};
-
-// @TODO Support themed values
-const gap = "1rem";
+import { cx } from "../styles";
 
 const Layout: React.FC = ({ children }) => (
   <>
     <Head />
-    <Box
-      as="header"
-      sx={{
-        paddingY: gap,
-        position: "sticky",
-        top: 0,
-        backgroundColor: "background",
-        ...border,
-        zIndex: 2,
-      }}
+    <header
+      className={cx(
+        "py-4",
+        "sticky",
+        "top-0",
+        "bg-background",
+        "z-10",
+        "u.border"
+      )}
     >
-      <Container
-        sx={{
-          display: ["block", "flex"],
-          alignItems: [null, "center"],
-          justifyContent: [null, "space-between"],
-        }}
+      <div
+        className={cx(
+          "c.container",
+          "sm:flex",
+          "sm:items-center",
+          "sm:justify-between"
+        )}
       >
-        <Heading as="h1" sx={{ fontSize: 5 }}>
-          <Link
-            href="#"
-            sx={{
-              "&:not(:hover):not(:focus)": {
-                color: "text",
-                textDecoration: "none",
-              },
-            }}
-          >
+        <h1 className={cx("c.heading", "text-4xl", "leading-9")}>
+          <a href="#" className={cx("c.navLink")}>
             raam
-          </Link>
-        </Heading>
+          </a>
+        </h1>
 
-        <Box
-          as="nav"
-          sx={{
-            display: "flex",
-            marginTop: [gap, 0],
-          }}
-        >
-          <Flexbox as="ul" alignItems="center" gap={3} variant="wrap">
+        <nav className={cx("flex", "mt-4", "sm:mt-0")}>
+          <ul
+            className={cx(
+              "flex",
+              "flex-row",
+              "flex-wrap",
+              "flex-gap-4",
+              "items-center"
+            )}
+          >
             {config.navigation.map((item) => (
-              <NavLink key={item.url} href={item.url}>
-                {item.title}
-              </NavLink>
+              <li key={item.url} className={cx("flex-initial")}>
+                <a className={cx("c.navLink")} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
             ))}
-          </Flexbox>
+          </ul>
 
           <ToggleColorMode
-            sx={{
-              position: ["absolute", "relative"],
-              top: [gap, 0],
-              right: [gap, 0],
-              marginLeft: [null, gap],
-            }}
+            className={cx(
+              "top-0",
+              "right-0",
+              "absolute",
+              "sm:relative",
+              "mt-4",
+              "sm:mt-0",
+              "mr-4",
+              "sm:mr-0",
+              "sm:ml-4"
+            )}
           />
-        </Box>
-      </Container>
-    </Box>
+        </nav>
+      </div>
+    </header>
 
-    <Box sx={{ ...border, borderTop: 0, paddingY: gap }}>
-      <Container as="main">{children}</Container>
-    </Box>
+    <div className={cx("u.border", "border-t-0", "py-4")}>
+      <main className={cx("c.container")}>{children}</main>
+    </div>
 
-    <Box sx={{ ...border, borderTop: 0, paddingY: gap }}>
-      <Container as="footer">
+    <div className={cx("u.border", "border-t-0", "py-4")}>
+      <footer className={cx("c.container")}>
         © 2020{" "}
-        <NavLink href={`https://twitter.com/${config.meta.social.twitter}`}>
+        <a
+          className={cx("c.navLink")}
+          href={`https://twitter.com/${config.meta.social.twitter}`}
+        >
           {config.meta.author}
-        </NavLink>
-      </Container>
-    </Box>
+        </a>
+      </footer>
+    </div>
   </>
 );
 
